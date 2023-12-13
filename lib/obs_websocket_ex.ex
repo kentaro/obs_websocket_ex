4ba5e2a6c5cf3c7260803ea48a06d96bec645599
Kentaro Kuribayashi <kentarok@gmail.com>
2023-12-13 15:39:41 +0900

    mix new obs_websocket_ex

---
+++ lib/obs_websocket_ex.ex
@@ -0,0 +1,18 @@
+defmodule ObsWebsocketEx do
+  @moduledoc """
+  Documentation for `ObsWebsocketEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ObsWebsocketEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/obs_websocket_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ObsWebsocketExTest do
+  use ExUnit.Case
+  doctest ObsWebsocketEx
+
+  test "greets the world" do
+    assert ObsWebsocketEx.hello() == :world
+  end
+end
